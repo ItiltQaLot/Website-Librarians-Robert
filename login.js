432e0cc97178570bc50e09bd0826f67f543101d3
ItiltQaLot <112977944+ItiltQaLot@users.noreply.github.com>
2023-04-20 23:31:36 -0400

    Create login.js

---
+++ login.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={handleEmailChange} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <button type="submit">Log In</button>
+    </form>
+  );
+}
+
+export default Login;
